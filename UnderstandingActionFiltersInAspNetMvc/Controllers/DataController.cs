157926e0070f1f1db52c25104409bf703bba2f08
Tarak-Chandra-Sarkar <tarak.chandra.sarkar@hotmail.com>
2018-12-26 16:31:33 +0530

    Implementation custom ErrorHandler ActionFilter

--- UnderstandingActionFiltersInAspNetMvc/Controllers/DataController.cs
+++ UnderstandingActionFiltersInAspNetMvc/Controllers/DataController.cs
@@ -5,12 +5,22 @@ using UnderstandingActionFiltersInAspNetMvc.Filters;
 namespace UnderstandingActionFiltersInAspNetMvc.Controllers
 {
     [LogActionFilter]
+    [MyCustomErrorHandler]
     public class DataController : Controller
     {
         [OutputCache(Duration=10)]
         public string Time()
         {
             return DateTime.Now.ToString("T");
+        }
+         
+        
+        public ActionResult Error()
+        {
+            // Throw a new Exception
+            throw new Exception("An Exception Occured at Error action of Data controller");
+
+            return View();
         }
     }
 }
---
+++ UnderstandingActionFiltersInAspNetMvc/Filters/MyCustomErrorHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnderstandingActionFiltersInAspNetMvc.Filters
+{
+    public class MyCustomErrorHandler : FilterAttribute, IExceptionFilter
+    {
+        void IExceptionFilter.OnException(ExceptionContext filterContext)
+        {
+            LogError(filterContext.Exception);
+        }
+
+        private void LogError(Exception exception)
+        {
+            var msg =exception.Message +Environment.NewLine + exception.StackTrace;
+
+            System.IO.File.AppendAllText(HttpContext.Current.Server.MapPath("~/App_Data/MyCustomErrorHandler.log"), msg + Environment.NewLine, System.Text.Encoding.UTF8);
+        }
+    }
+}
